4c645f7b5d833a7941bcf005d7d21efd9a5bba8c
akshayengr <akshaysingh.engr@gmail.com>
2021-09-02 10:42:56 +0530

    adding hello world script as sample python file

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  2 10:36:16 2021
+
+@author: aksha
+"""
+
+def main():
+    
+    return "Hello world"
+
+if __name__ == "__main__":
+    print(main())
